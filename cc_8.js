7c41f86fad63439f5f64a37b258c1faca2fbebd8
Fotihjon <ikromov@usf.edu>
2025-02-16 17:57:48 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -16,3 +16,21 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Test Cases
 console.log(calculateSalary(5000, 500, 0.1)); // "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // "Net Salary: $6950.00"
+
+//Task 2: Product Price After Discount
+
+/*
+A function expression named calculateDiscount computes the final price.
+Formula:
+Final Price = Price - (Price * Discount Rate)
+*/
+
+// Task 2: Product Price After Discount
+const calculateDiscount = function(price, discountRate) {
+  let finalPrice = price - (price * discountRate);
+  console.log(`Final Price: $${finalPrice.toFixed(2)}`);
+};
+
+// Test cases
+calculateDiscount(100, 0.2); // Expected: Final Price: $80.00
+calculateDiscount(250, 0.15); // Expected: Final Price: $212.50
